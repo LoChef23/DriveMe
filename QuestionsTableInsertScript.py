5442aba6c0d4b43724f97f96d83369d4ddef5269
LoChef23 <giulio.penna@hotmail.it>
2019-11-30 10:23:24 +0100

    revised insert script

--- QuestionsTableInsertScript.py
+++ QuestionsTableInsertScript.py
@@ -1,23 +1,5 @@
 import json
-import boto3
-
-class Question():
-
-    def __init__(self, questionID, questionText, isCorrect):
-        self.questionID = questionID
-        self.questionText = questionText
-        self.isCorrect = isCorrect
-
-    def import_in_dynamodb(self):
-        client = boto3.client('dynamodb')
-        importQuestion = client.put_item(
-            TableName = 'Questions',
-            Item={
-                'QuestionID':{'N': self.questionID},
-                'QuestionText':{'S': self.questionText},
-                'isCorrect':{'BOOL': self.isCorrect}
-            }
-        )
+from Question import Question
 
 with open('questions.json', 'r') as questionsFile:
     questionsList = json.load(questionsFile)
