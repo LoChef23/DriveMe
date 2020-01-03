8dcbba528da35b78ec99010a17a7ae8e3528bfcc
LoChef23 <giulio.penna@hotmail.it>
2020-01-03 18:12:19 +0100

    changed response and added app info

--- api/get.py
+++ api/get.py
@@ -8,7 +8,9 @@ tableDesc = client.describe_table(TableName='Questions')
 itemsInTable = int(tableDesc['Table']['ItemCount'])
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app,version='0.1',
+              title='DriveMe',
+              description='App for anyone who wants to make some practice with some simple Quizzes in order to prepare the Driving License exam')
 
 @api.route('/question/')
 class Question(Resource):
@@ -20,7 +22,15 @@ class Question(Resource):
                 'QuestionID': {'N': str(questionRandomID)}
             }
         )
-        response = result['Item']
+        print(result)
+        response = {}
+        response['Question'] = {}
+        response['Question']['ID'] = result['Item']['QuestionID']['N']
+        response['Question']['Text'] = str(result['Item']['QuestionText']['S'])
+        if result['Item']['QuestionExplanation']['S'] != '-':
+            response['Question']['Explanation'] = result['Item']['QuestionExplanation']['S']
+        if result['Item']['QuestionImage']['S'] != '-':
+            response['Question']['Image'] = result['Item']['QuestionImage']['S']
         return response
 
 if __name__ == '__main__':
